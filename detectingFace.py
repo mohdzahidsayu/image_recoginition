e8087eee6f6e090af6baed59cd5d6ece375c767a
_mohd._.zahid_ <56506601+mohdzahidsayu@users.noreply.github.com>
2020-10-10 21:22:20 +0530

    detecting Face
    using haarcascade_frontalface_default

---
+++ detectingFace.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+
+alg = "haarcascade_frontalface_default.xml"
+haar = cv2.CascadeClassifier(alg)
+
+cam = cv2.VideoCapture(0)
+dataset = "dataset"
+name = "champ"
+
+path = os.path.join(dataset,name)
+if not os.path.isdir(path):
+		os.mkdir(path)
+(width,height) = (130,100)
+count = 1		
+
+while (count < 30):
+	_,img = cam.read()
+	grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	faces = haar.detectMultiScale(grayImg,1.3,4)
+	
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img, (x,y), (x+w , y+h),(0,255,0),2)
+		onlyFace = grayImg[y:y+h,x:x+w]
+		resizeImg = cv2.resize(grayImg,(width,height))
+		cv2.imwrite("%s/person%s.jpg" %(path,count),resizeImg)
+		count+=1
+
+	cv2.imshow("person",img)
+	key = cv2.waitKey(10)
+	
+	if key == 29:
+		break
+print("Face captured successfully")
+cam.release()
+cv2.destroyAllWindows()
+		 
